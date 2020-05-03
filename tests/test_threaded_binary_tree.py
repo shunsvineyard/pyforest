a724abcb309d2fd0133c6a9d291c2ca7d916c645
Shun Huang <shunsvineyard@protonmail.com>
2020-05-03 00:11:59 -0700

    Simplified threaded binary tree unit tests.

--- tests/test_threaded_binary_tree.py
+++ tests/test_threaded_binary_tree.py
@@ -13,11 +13,8 @@ def test_simple_right_threaded_case(basic_tree):
     for key, data in basic_tree:
         tree.insert(key=key, data=data)
 
-    output = ["1", "4", "7", "11", "15", "20", "22", "23", "24", "30", "34"]
-    index = 0
-    for data in tree.inorder_traverse():
-        assert data == output[index]
-        index += 1
+    assert ["1", "4", "7", "11", "15", "20", "22", "23", "24", "30", "34"] == \
+           [item for item in tree.inorder_traverse()]
 
     assert tree.get_min() == 1
     assert tree.get_max() == 34
@@ -31,11 +28,8 @@ def test_simple_right_threaded_case(basic_tree):
     with pytest.raises(KeyError):
         tree.search(15)
 
-    output_after_deleted = ["1", "4", "11", "23", "24", "30", "34"]
-    index = 0
-    for data in tree.inorder_traverse():
-        assert data == output_after_deleted[index]
-        index += 1
+    assert ["1", "4", "11", "23", "24", "30", "34"] == \
+           [item for item in tree.inorder_traverse()]
 
 
 def test_deletion_right_threaded_case(basic_tree):
@@ -48,36 +42,24 @@ def test_deletion_right_threaded_case(basic_tree):
 
     # No child
     tree.delete(15)
-    no_child_output = ["1", "4", "7", "11", "20", "22", "23", "24", "30", "34"]
-    index = 0
-    for data in tree.inorder_traverse():
-        assert data == no_child_output[index]
-        index += 1
+    assert ["1", "4", "7", "11", "20", "22", "23", "24", "30", "34"] == \
+           [item for item in tree.inorder_traverse()]
 
     # One right child
     tree.delete(20)
-    one_right_child_output = ["1", "4", "7", "11", "22", "23", "24", "30", "34"]
-    index = 0
-    for data in tree.inorder_traverse():
-        assert data == one_right_child_output[index]
-        index += 1
+    assert ["1", "4", "7", "11", "22", "23", "24", "30", "34"] == \
+           [item for item in tree.inorder_traverse()]
 
     # One left child
     tree.insert(key=17, data="17")
     tree.delete(22)
-    one_left_child_output = ["1", "4", "7", "11", "17", "23", "24", "30", "34"]
-    index = 0
-    for data in tree.inorder_traverse():
-        assert data == one_left_child_output[index]
-        index += 1
+    assert ["1", "4", "7", "11", "17", "23", "24", "30", "34"] == \
+           [item for item in tree.inorder_traverse()]
 
     # Two children
     tree.delete(11)
-    two_children_output = ["1", "4", "7", "17", "23", "24", "30", "34"]
-    index = 0
-    for data in tree.inorder_traverse():
-        assert data == two_children_output[index]
-        index += 1
+    assert ["1", "4", "7", "17", "23", "24", "30", "34"] == \
+           [item for item in tree.inorder_traverse()]
 
 
 def test_simple_left_threaded_case(basic_tree):
@@ -88,11 +70,9 @@ def test_simple_left_threaded_case(basic_tree):
     for key, data in basic_tree:
         tree.insert(key=key, data=data)
 
-    output = ["34", "30", "24", "23", "22", "20", "15", "11", "7", "4", "1"]
-    index = 0
-    for data in tree.outorder_traverse():
-        assert data == output[index]
-        index += 1
+    assert ["34", "30", "24", "23", "22", "20", "15", "11", "7", "4", "1"] == \
+           [item for item in tree.outorder_traverse()]
+
 
 def test_simple_double_threaded_case(basic_tree):
     """Test the basic opeartions of a double threaded binary search tree."""
@@ -102,14 +82,8 @@ def test_simple_double_threaded_case(basic_tree):
     for key, data in basic_tree:
         tree.insert(key=key, data=data)
 
-    outorder_output = ["34", "30", "24", "23", "22", "20", "15", "11", "7", "4", "1"]
-    index = 0
-    for data in tree.outorder_traverse():
-        assert data == outorder_output[index]
-        index += 1
+    assert ["34", "30", "24", "23", "22", "20", "15", "11", "7", "4", "1"] == \
+           [item for item in tree.outorder_traverse()]
 
-    inorder_output = ["1", "4", "7", "11", "15", "20", "22", "23", "24", "30", "34"]
-    index = 0
-    for data in tree.inorder_traverse():
-        assert data == inorder_output[index]
-        index += 1
+    assert ["1", "4", "7", "11", "15", "20", "22", "23", "24", "30", "34"] == \
+           [item for item in tree.inorder_traverse()]
