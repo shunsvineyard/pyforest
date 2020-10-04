ba8f6ec7d7c80a9f396a22423d9f9d66cc362d64
Shun Huang <shunsvineyard@protonmail.com>
2020-10-03 23:58:16 -0700

    Bump up version.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019 Shun Huang'
 author = 'Shun Huang'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
